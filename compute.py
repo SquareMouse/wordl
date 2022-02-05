fa419fefb0f54a0e4258686dd3b3c8a2d6f00496
Jay Nagin <jknagin@gmail.com>
2022-02-05 13:29:40 -0800

    Formatting, fixing typos

--- compute.py
+++ compute.py
@@ -3,14 +3,16 @@ from enum import Enum
 from collections import defaultdict
 from dataclasses import dataclass
 
+
 class Color(Enum):
-    GRAY   = 0
+    GRAY = 0
     YELLOW = 1
-    GREEN  = 2
+    GREEN = 2
+
 
 class Word(defaultdict):
     def __init__(self, s: str):
-        super().__init__(lambda:set())
+        super().__init__(lambda: set())
         for idx, letter in enumerate(s):
             self[letter].add(idx)
         self._data = s
@@ -20,7 +22,8 @@ class Word(defaultdict):
 
     def __repr__(self):
         return self.__str__()
-    
+
+
 @dataclass(frozen=True)
 class Filter:
     a: Color
@@ -31,7 +34,7 @@ class Filter:
 
     def __hash__(self):
         return int(self.a.value + 3 * self.b.value + 9 * self.c.value + 27 * self.d.value + 81 * self.e.value)
-            
+
     @classmethod
     def compute(cls, query: Word, secret: Word):
         filter = [Color.GRAY] * 5
@@ -42,7 +45,7 @@ class Filter:
             greenIdxSet = secretIdxSet.intersection(queryIdxSet)
             for greenIdx in greenIdxSet:
                 filter[greenIdx] = Color.GREEN
-            
+
             numYellow = min(len(secretIdxSet) - len(greenIdxSet), len(queryIdxSet) - len(greenIdxSet))
             if numYellow == 0:
                 continue
@@ -50,11 +53,13 @@ class Filter:
                 filter[yellowIdx] = Color.YELLOW
         return cls(*filter)
 
+
 def compute(query: Word, secretCandidates: List[Word]) -> Dict[Filter, List[Word]]:
     computed = defaultdict(lambda: [])
     for secret in secretCandidates:
         computed[Filter.compute(query, secret)].append(secret)
     return computed
+
 
 def findBestWord(queryCandidates, secretCandidates):
     output = []
@@ -65,13 +70,14 @@ def findBestWord(queryCandidates, secretCandidates):
     output.sort(key=lambda x: x[0])
     return output
 
+
 def filterFromInput() -> Filter:
-    print("Enter lowerspaced colors with spaces in between: ", end='')
+    print("Enter lowercase colors with spaces in between: ", end='')
     i = input().split(" ")
     if len(i) != 5:
-        print("entier 5 colors separated with spaces")
+        print("enter 5 colors separated with spaces")
         return None
-    strToEnum = {"green":Color.GREEN, "gray":Color.GRAY, "yellow":Color.YELLOW}
+    strToEnum = {"green": Color.GREEN, "gray": Color.GRAY, "yellow": Color.YELLOW}
     enums = []
     for c in i:
         if c not in strToEnum:
@@ -80,24 +86,23 @@ def filterFromInput() -> Filter:
         enums.append(strToEnum[c])
     return Filter(*enums)
 
+
 with open("sgb-words.txt") as f:
     wordBank = [Word(s) for s in sorted(f.read().splitlines())]
-
-assert Filter.compute(Word("oooll"), Word("llool")) == Filter(Color.YELLOW,Color.GRAY,Color.GREEN,Color.YELLOW,Color.GREEN)
 
 optimalFirstWord = Word("aloes")
 
 if __name__ == "__main__":
-    secretSpace = wordBank
-    query = optimalFirstWord
-    filt = None
+    secretSpace: List[Word] = wordBank
+    query: Word = optimalFirstWord
+    filt: Filter = None
     while len(secretSpace) > 1:
         print("Next Word To Try:", query)
         filterToSecretSpace = compute(query, secretSpace)
-        while(not filt):
+        while not filt:
             filt = filterFromInput()
         secretSpace = filterToSecretSpace[filt]
         filt = None
         query = findBestWord(wordBank, secretSpace)[0][1]
-        
+
     print("Found:", secretSpace[0])
